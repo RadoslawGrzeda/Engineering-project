3bdeada195de10ff37e330e5d17d972284ebe11c
Radosław Grzeda <radoslaw@example.com>
2026-02-07 23:01:11 +0100

    Enhance data processing module by adding Chief model and improving database loading logic
    - Introduced `Chief` model in `shema.py` for better data structure.
    - Updated `main.py` to include `source_file` in database insertions for `sector`, `department`, and `segment`.
    - Implemented validation for existing sectors before loading departments and segments.
    - Improved error logging for invalid sector IDs during data loading.

--- apps/load_file_develop/shema.py
+++ apps/load_file_develop/shema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field, EmailStr ,field_validator
+from pydantic_extra_types.phone_numbers import PhoneNumber
 from typing import List, Optional   
 import datetime
 import re
@@ -22,25 +23,25 @@ class Segment(BaseModel):
     # chief_id: str = Field(..., alias='chief_id')
     sector_id: int = Field(..., gt=0)
 
-"""
 class Chief(BaseModel):
-    chief_id: str = Field(..., alias='chief_id')
-    segment_id: str = Field(..., alias='segment_id')
-    chief_first_name: str = Field(...,min_length=1,max_length=50, alias='chief_first_name')
-    chief_last_name: str = Field(...,min_length=1,max_length=50, alias='chief_last_name')
-    chief_email: str = Field(...,min_length=5,max_length=100, alias='chief_email')
-    chief_phone: str = Field(...,min_length=5,max_length=20, alias='chief_phone')
-    is_current: bool = Field(..., alias='is_current')
-    date_start: datetime.date = Field(..., alias='date_start')
-    date_end: Optional[datetime.date] = Field(None, alias='date_end')
-# chief_email,chief_phone,is_current,date_start,date_end
-    @field_validator('date_end',mode='before')
+    chief_id: str = Field(...)
+    # segment_id: int = Field(..., gt=0)
+    chief_first_name: str = Field(...,min_length=1,max_length=50)
+    chief_last_name: str = Field(...,min_length=1,max_length=50)
+    chief_email: str = Field(...,min_length=5,max_length=100)
+    chief_phone: PhoneNumber = Field(...)
+    # is_current: bool = Field(...)
+    # date_start: datetime.date = Field(...)
+    # date_end: Optional[datetime.date] = Field(None)
+
+
+    @field_validator('chief_id')
     @classmethod
-    def validate_date_end(cls, v):
-        if v in (None, ''):
-            return None
-        return v
-    
+    def validate_chief_id(cls, v):
+        try:
+            return v if re.match(r'^CH[0-9]{3}$', v) else ValueError(f"Invalid chief id format: {v}")
+        except Exception as e:
+            raise ValueError(f"Invalid chief id format: {v}. Error: {str(e)}")
 
     @field_validator('chief_email')
     @classmethod
@@ -51,6 +52,16 @@ class Chief(BaseModel):
         except EmailNotValidError as e:
             raise ValueError(f"Invalid email address: {v}. Error: {str(e)}")
 
+'''
+    @field_validator('date_end',mode='before')
+    @classmethod
+    def validate_date_end(cls, v):
+        if v in (None, ''):
+            return None
+        return v
+    
+
+ 
     @field_validator("is_current", mode="before")
     @classmethod
     def parse_bool(cls, v):
@@ -61,7 +72,7 @@ class Chief(BaseModel):
             if vv in ("0", "false", "f", "no", "n"):
                 return False
         return v
-
+'''
 
 class Category(BaseModel):
     category_id: str = Field(..., alias='category_id')
