335b63110d4031cef7ecd883e2fe460051f4cdc0
Radosław Grzeda <radoslaw@example.com>
2026-02-11 13:54:04 +0100

    Enhance data processing functionality by adding contractor loading and validation
    - Introduced `_load_contractor` method in `main.py` to handle contractor data insertion with conflict resolution.
    - Updated `process_data` class to include logic for updating `segment_chief` records and inserting new relationships.
    - Added validation for `chief_id` and `segment_code` in the `Segment` model within `shema.py` to enforce proper formatting.
    - Improved logging for database operations to track updates and insertions effectively.

--- apps/load_file_develop/main.py
+++ apps/load_file_develop/main.py
@@ -152,7 +152,6 @@ class process_data:
         valid = df[df['sector_id'].isin(existing_sectors)].copy()
 
         source_file=self.path
-
         sql=text(f"""
                     INSERT INTO segment (segment_id, segment_code, segment_name, sector_id, source_file)
                     VALUES (:segment_id, :segment_code, :segment_name, :sector_id, :source_file)
@@ -163,7 +162,38 @@ class process_data:
                         source_file = EXCLUDED.source_file;
                     """)
 
-        records=valid.to_dict(orient='records')
+        update_sql=text(f'''
+                        UPDATE segment_chief 
+                        SET is_current = False,
+                        valid_to=current_date
+                        where segment_id = :segment_id
+                        and chief_id != :chief_id
+                        and valid_to is null;
+
+                        ''')        
+
+        insert_relation_sql = text(f'''
+                                INSERT INTO segment_chief (segment_id, chief_id, is_current, valid_from, valid_to, source_file)
+                                VALUES (:segment_id, :chief_id, :is_current, :valid_from, :valid_to, :source_file)
+                                ON CONFLICT (segment_id, chief_id) DO UPDATE SET
+                                is_current = True,
+                                valid_from = current_date,
+                                valid_to = NULL,
+                                source_file = EXCLUDED.source_file;
+                                
+                                ''')
+        updateRecords=valid.copy().to_dict(orient='records')
+        source_file=self.path
+        for record in updateRecords:
+            record['source_file']=source_file
+            record['valid_from']=datetime.date(2023,1,1)
+            record['valid_to']=None
+            record['is_current']=True
+
+
+
+
+        records=valid.copy().to_dict(orient='records')
         for record in records:
             record['source_file']=source_file
 
@@ -172,9 +202,17 @@ class process_data:
             
         with self.engine.begin() as conn:
             try: 
+                logging.info(f"Updating records in segment_chief...")
+                conn.execute(update_sql, updateRecords)
+                logging.info(f"Updated segment_chief records successfully.")
+
                 logging.info(f"Inserting records into segment...")
                 conn.execute(sql, records)
                 logging.info(f"Inserted segment records successfully.")
+
+                logging.info(f"Inserting records into segment_chief...")
+                conn.execute(insert_relation_sql, updateRecords)
+                logging.info(f"Inserted segment_chief records successfully.")
             except Exception as e:
                 logger.error(f"Error inserting records into segment: {e}")
 
@@ -197,36 +235,47 @@ class process_data:
         df_chief=df.copy()
         df_chief['source_file']=source_file
 
-        # df_chief=df[['chief_id', 'chief_first_name', 'chief_last_name', 'chief_email', 'chief_phone']].copy()
-        # df_chief=df_chief.drop_duplicates(subset=['chief_id'], keep='last')
-        
-        # df_segment_chief=df[['segment_id', 'chief_id','is_current','date_start','date_end']].copy()
-        # df_segment_chief=df_segment_chief.drop_duplicates(subset=['segment_id'], keep='last')
-
         with self.engine.begin() as conn:
             try: 
                 logging.info(f"Inserting records into chief...")
-
                 record=df_chief.to_dict(orient='records')
                 conn.execute(sql, record)
                 logging.info(f"Inserted chief records successfully.")
-                
-                # ## update segment_chief
-                # conn.execute(sqlUpdate, [{'today': today, **rec} for rec in df_segment_chief.to_dict(orient='records')])
-                # logging.info(f"Updated segment_chief records successfully, start insertion into segment_chief...")
-                
-                # ## segment_chief
-                # record_segment_chief=df_segment_chief.to_dict(orient='records')                                
-                # conn.execute(sqlINSERT, record_segment_chief)
-                # logging.info(f"Inserted record_segment_chief  successfully.")
 
             except Exception as e:
                 logger.error(f"Error inserting records into chief: {e}")
 
-path='/Users/radoslaw/Desktop/Engineering-project/apps/load_file_develop/data/new_chief.csv'
+    
+    def _load_contractor(self, df: pd.DataFrame) -> None:
+
+        # contractor_id,contractor_name,brand,contact_phone_number,contact_phone_email,address
+        sql = text(f'''
+        INSERT INTO contractor (contractor_id, contractor_name, contractor_email, contractor_phone, source_file)
+        VALUES (:contractor_id, :contractor_name, :contractor_email, :contractor_phone, :source_file)
+        ON CONFLICT (contractor_id) DO UPDATE SET
+        contractor_name = EXCLUDED.contractor_name,
+        contractor_email = EXCLUDED.contractor_email,
+        contractor_phone = EXCLUDED.contractor_phone,
+        source_file = EXCLUDED.source_file;
+        ''')
+        source_file=self.path
+
+        with self.engine.begin() as conn:
+            try:
+                logging.info(f"Inserting records into contractor...")
+                record=df.to_dict(orient='records')
+                record['source_file']=source_file
+                conn.execute(sql, record)
+                logging.info(f"Inserted contractor records successfully.")
+            except Exception as e:
+                logger.error(f"Error inserting records into contractor: {e}")
+
+
+
+path='/Users/radoslaw/Desktop/Engineering-project/apps/load_file_develop/data/segment.csv'
 process_data=process_data(path)
-df_chief, errors=process_data.validate_shape(Chief)
-process_data.load_to_db(df_chief, 'chief')
+df_segment, errors=process_data.validate_shape(Segment)
+process_data.load_to_db(df_segment, 'segment')
 # print(errors)
 
 
