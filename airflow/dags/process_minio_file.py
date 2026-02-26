ea0d98e6c46e0a87be915262d37c48760fc2a3a0
Radosław Grzeda <radoslaw@example.com>
2026-02-26 21:49:23 +0100

    Implement Minio file processing DAG with schema validation and database loading

---
+++ airflow/dags/process_minio_file.py
@@ -0,0 +1,61 @@
+# from apps.kafka_minio_consumer.main import file_key
+# from airflow.operators.python import PythonOperator
+from airflow.decorators import task, dag
+from datetime import datetime
+
+
+@dag(
+    dag_id='process_minio_file',
+    schedule=None,
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+    tags=['minio', 'etl']
+)
+def process_minio_file():
+    @task
+    def load_file_from_bucket(**args):
+        from minio import Minio
+        import pandas as pd
+        import os, tempfile
+
+        bucket_name = args['bucket_name']
+        file_name = args['file_name']
+        file_key = args['file_key']
+        file_schema=args['schema']
+
+        minio_client=Minio (
+            os.getenv('MINIO_ADDRESS'),
+            access_key=os.getenv('MINIO_USER'),
+            secret_key=os.getenv('MINIO_PASSWORD'),
+            secure=False
+        )
+        local_path=os.path.join(tempfile.gettempdir(),file_key.split('/')[-1])
+        minio_client.fget_object(bucket_name, file_key, local_path)
+        print(f"File downloaded to {local_path}")
+        return {'path':local_path, 'file_schema': file_schema}
+    
+    @task
+    def validate_schema_and_load_file(file_info):
+        from apps.load_file_develop.main import ProcessData
+        try:
+            process_data=ProcessData(file_info['path'])
+            data, errors = process_data.validate_shape(file_info['file_schema'])
+        except Exception as e:
+            print(f"Error during schema validation: {e}")
+            raise   
+        if errors:
+            print(f"Schema validation errors: {errors}")
+            raise ValueError("Schema validation failed")
+        print("Schema validation passed")
+            # return process_data,file_info['file_schema'], data
+        try:
+            process_data.load_to_db(data, file_info['file_schema'])
+        except Exception as e:
+            print(f"Error during loading to database: {e}")
+            raise
+        print("Data loaded successfully")
+    
+    file_info = load_file_from_bucket()
+    validate_schema_and_load_file(file_info)
+
+process_minio_file()
