8cbdba5194c8aad80a7596e21e5c69ce5bed2c83
Radosław Grzeda <radoslaw@example.com>
2026-03-04 22:43:49 +0100

    Enhance logging configuration: add correlation ID support and update log format to include correlation ID

--- apps/logger_config.py
+++ apps/logger_config.py
@@ -1,5 +1,13 @@
 import logging
 from pythonjsonlogger import json as jsonlogger
+from contextvars import ContextVar
+
+correlation_id:ContextVar[str] = ContextVar('corellation_id',default='-')
+
+class CorrelationFilter(logging.Filter):
+    def filter(self, record):
+        record.correlation_id = correlation_id.get()
+        return True
 
 def get_logger(name : str) -> logging.Logger:
     logger = logging.getLogger(name)
@@ -11,11 +19,12 @@ def get_logger(name : str) -> logging.Logger:
 
     handler = logging.StreamHandler()
     formatter = jsonlogger.JsonFormatter(
-        fmt="%(asctime)s %(levelname)s %(name)s %(message)s",
+        fmt="%(asctime)s %(levelname)s %(name)s %(message)s %(correlation_id)s",
         datefmt="%Y-%m-%dT%H:%M:%S",
         rename_fields={"asctime": "timestamp", "levelname": "level", "name": "logger"},
     )
     handler.setFormatter(formatter)
+    handler.addFilter(CorrelationFilter())
     logger.addHandler(handler)
 
     return logger
