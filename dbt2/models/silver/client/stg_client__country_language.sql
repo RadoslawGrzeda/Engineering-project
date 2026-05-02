153a9e09e91a94dbfd8d254f4e1964ac3173f5a4
RadoslawGrzeda <radoslaw.grzeda@pjwstk.edu.pl>
2026-05-02 10:31:24 +0200

    Delete dbt2 directory

--- dbt2/models/silver/client/stg_client__country_language.sql
+++
@@ -1,2 +0,0 @@
-select *
-from {{ source('bronze', 'client__country_language') }}
--- dbt2/models/silver/client/stg_client__dict_contact.sql
+++
@@ -1,6 +0,0 @@
-select 
-    contact_type,
-    contact_name,
-    contact_description,
-    validation_regex,
-from {{ source('bronze', 'client__dict_contact') }}
